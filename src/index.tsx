93d89b9468ee04005f9ab3207f42943e7b778ca3
Noé <noe_juarez@epam.com>
2022-04-05 22:04:35 -0500

    [INIT]

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+      <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
